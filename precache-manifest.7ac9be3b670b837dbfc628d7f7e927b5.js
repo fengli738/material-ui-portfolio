388dc9648b8b3bd296d8754494e596c297129307
Feng Li <fengli738@gmail.com>
2023-01-06 14:31:06 -0600

    Updates

---
+++ precache-manifest.7ac9be3b670b837dbfc628d7f7e927b5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e2971249b77358116a1c611217947a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4a705eccf701574b7b3",
+    "url": "/static/css/main.6fc31ce3.chunk.css"
+  },
+  {
+    "revision": "8b16a7a27d437a9ce903",
+    "url": "/static/js/2.f980019c.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.f980019c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a705eccf701574b7b3",
+    "url": "/static/js/main.0e08d12c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "d698dfc696daecccf2eb241891a929a6",
+    "url": "/static/media/avatar.d698dfc6.JPG"
+  },
+  {
+    "revision": "715176c11842de609ea44267788aa0e9",
+    "url": "/static/media/elexon_tube_2400x1200.715176c1.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
